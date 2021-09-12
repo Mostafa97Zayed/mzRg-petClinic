89fa8c96d370669b752b4df7a197c94ae6c6da2d
mostafa zayed <mostafa.zayed97@gmail.com>
2021-09-12 22:18:10 +0200

    FIRST

---
+++ src/main/java/mostafa/springframework/mzRgpetClinic/MzRgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package mostafa.springframework.mzRgpetClinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MzRgPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MzRgPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mostafa/springframework/mzRgpetClinic/MzRgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package mostafa.springframework.mzRgpetClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MzRgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
