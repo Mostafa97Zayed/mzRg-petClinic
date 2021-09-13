9a6dc0407df578f62864c2776f81bc29a24696a1
mostafa zayed <mostafa.zayed97@gmail.com>
2021-09-13 10:24:04 +0200

    multi mavem. closes #1

--- pet-clinic-web/src/test/java/mostafa/springframwrokk/mzRgpetClinic/MzRgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/mostafa/springframwrokk/mzRgpetClinic/MzRgPetClinicApplicationTests.java
@@ -1,4 +1,4 @@
-package mostafa.springframework.mzRgpetClinic;
+package mostafa.springframwrokk.mzRgpetClinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
